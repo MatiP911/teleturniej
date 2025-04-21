095a9721221e7ae1e4d67677947633e124f38733
Fred <64082311+MatiP911@users.noreply.github.com>
2025-04-21 23:27:51 +0200

    addded central buttons and initialscore

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ pallet = {
     "bg": "black"
 }
 
+
 score = (0, 0)
 
 
@@ -21,8 +22,12 @@ class App(ctk.CTk):
         self.configure(fg_color=pallet['bg'])
 
         self.columnconfigure((0, 2), weight=1, uniform='a')
-        self.columnconfigure(1, weight=4)
+        self.columnconfigure(1, weight=2)
         self.rowconfigure(0, weight=2, uniform='a')
+
+        # create score
+        self.scoreVar = (ctk.StringVar(value=""), ctk.StringVar(value=""))
+        self.updateScoreView()
 
         # Wigets
         self.teamsA = teamView(self, 0)
@@ -33,9 +38,13 @@ class App(ctk.CTk):
 
         # Maingame
         self.gameScreen = mainGame(self)
-        self.gameScreen .grid(row=0, column=1, sticky='nswe')
+        self.gameScreen .grid(row=0, column=1, sticky='nswe', padx=40, pady=40)
 
         self.mainloop()
+
+    def updateScoreView(self):
+        self.scoreVar[0].set(f"{score[0]}")
+        self.scoreVar[1].set(f"{score[1]}")
 
 
 class teamView(ctk.CTkFrame):
@@ -43,18 +52,33 @@ class teamView(ctk.CTkFrame):
         super().__init__(parent)
         self.configure(fg_color="transparent")
 
-        self.columnconfigure(0, weight=1, uniform='a')
-        self.rowconfigure(0, weight=1, uniform='a')
-        self.rowconfigure(1, weight=8, uniform='a')
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=8)
 
-        scoreViewer = ctk.CTkLabel(self, text_color=pallet['team'][teamNumber])
+        scoreViewer = ctk.CTkLabel(self, text_color=pallet['team'][teamNumber],
+                                   textvariable=parent.scoreVar[teamNumber],
+                                   font=('Tekton Pro', 40))
         scoreViewer.grid(row=0, column=0, sticky='nswe')
 
 
 class mainGame(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
-        self.configure(fg_color="transparent", border_width=40)
+        self.configure(fg_color='transparent')
+        self.columnconfigure((0, 1, 2, 3, 4), weight=1, uniform='a')
+        self.rowconfigure((0, 1, 2, 3, 4), weight=1, uniform='a')
+
+        for i in range(5):
+            for j in range(5):
+                cell = Question(self, (i, j))
+                cell.grid(row=i, column=j, sticky='nswe', padx=5, pady=5)
+
+
+class Question(ctk.CTkFrame):
+    def __init__(self, parent, id):
+        super().__init__(parent)
+        self.configure(border_width=5)
 
 
 if __name__ == '__main__':
