8bdecd43cdf1542822303a3bd242dcf98d080513
Fred <64082311+MatiP911@users.noreply.github.com>
2025-04-21 22:59:39 +0200

    added basic layout

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import customtkinter as ctk
 
+pallet = {
+    "main":  "black",
+    "second":  "blue",
+    "third": "purple",
+    "team": ("green", "blue"),
+    "wrong": "red",
+    "none": "grey",
+    "bg": "black"
+}
+
+score = (0, 0)
+
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.geometry("1920x1080")
         self.title("Teleturniej")
-        # self.configure(fg_color=BGCOL)
+        self.configure(fg_color=pallet['bg'])
 
         self.columnconfigure((0, 2), weight=1, uniform='a')
         self.columnconfigure(1, weight=4)
         self.rowconfigure(0, weight=2, uniform='a')
 
         # Wigets
-        self.teamsA = teamView(self)
+        self.teamsA = teamView(self, 0)
         self.teamsA.grid(row=0, column=0, sticky='nswe')
 
-        self.teamsB = teamView(self)
+        self.teamsB = teamView(self, 1)
         self.teamsB.grid(row=0, column=2, sticky='nswe')
 
         # Maingame
@@ -27,14 +39,22 @@ class App(ctk.CTk):
 
 
 class teamView(ctk.CTkFrame):
-    def __init__(self, parent):
-        super().__init()
-        self.configure(fg_color = "blue")
+    def __init__(self, parent, teamNumber):
+        super().__init__(parent)
+        self.configure(fg_color="transparent")
+
+        self.columnconfigure(0, weight=1, uniform='a')
+        self.rowconfigure(0, weight=1, uniform='a')
+        self.rowconfigure(1, weight=8, uniform='a')
+
+        scoreViewer = ctk.CTkLabel(self, text_color=pallet['team'][teamNumber])
+        scoreViewer.grid(row=0, column=0, sticky='nswe')
 
 
 class mainGame(ctk.CTkFrame):
     def __init__(self, parent):
-        super().__init()
+        super().__init__(parent)
+        self.configure(fg_color="transparent", border_width=40)
 
 
 if __name__ == '__main__':
